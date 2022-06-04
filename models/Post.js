408dcabb50167ac68153c355048aaa289ae99a64
IsaiasQB <isaias.qb@hotmail.com>
2022-06-04 17:53:51 -0400

    defining the models

--- models/Post.js
+++ models/Post.js
@@ -4,7 +4,30 @@ const sequelize = require('../config/connection');
 class Post extends Model {}
 
 Post.init({
-
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [1]
+    }
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'user',
+      key: 'id'
+    }
+  }
 },
 {
   sequelize,
---
+++ models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+const Likes = require('./Likes');
+
+module.exports = { User, Post, Comment, Likes };
