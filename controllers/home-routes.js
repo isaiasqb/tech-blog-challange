73e4a68f5a826f2bd5915eaf8c7e9355e7ecfbb0
IsaiasQB <isaias.qb@hotmail.com>
2022-06-06 23:02:09 -0400

    added a homepage and further styling

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,44 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment, Likes } = require('../models');
+
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  Post.findAll({
+    attributes: ['id', 
+                  'title', 
+                  'content',
+                  'date_posted',
+                  [
+      sequelize.literal('(SELECT COUNT(*) FROM likes WHERE post.id = likes.post_id)'),
+      'likes_count'
+                  ]
+                ],
+    order: [['date_posted', 'DESC']],
+    include: [
+      {
+      model: User,
+      attributes: ['id', 'username']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_content', 'post_id', 'user_id', 'date_posted'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+    ]
+  })
+  .then(postInfo => {
+    const post = postInfo.map(postItem => postItem.get({ plain: true }))
+    res.render('homepage', { post });
+
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 
