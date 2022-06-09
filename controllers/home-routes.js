20d1342f94c27a0b6a18d8949c98b421b303555d
IsaiasQB <isaias.qb@hotmail.com>
2022-06-09 00:50:11 -0400

    single post view implemented

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -54,4 +54,49 @@ if (req.session.loggedIn) {
 });
 
 
+//SINGLE POST VIEW
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 
+                  'title',
+                  'content', 
+                  'date_posted',
+                  [
+                    sequelize.literal('(SELECT COUNT(*) FROM likes WHERE post.id = likes.post_id)'),
+                    'likes_count'
+                  ]],
+    include: [
+      {
+      model: User,
+      attributes: ['id', 'username']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_content', 'post_id', 'user_id', 'date_posted'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+    ]
+  })
+  .then(postInfo => {
+    if(!postInfo) {
+      res.status(404).json({ message:'No post Found with this id' });
+      return
+    }
+
+    const post = postInfo.get({ plain: true });
+
+    res.render('single-post', { post });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+});
+
 module.exports = router;
